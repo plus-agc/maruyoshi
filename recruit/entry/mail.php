7e7b5fe1215e8ceb6d5302e0731b111eb779f2df
plus-agc-service-yamauchi <yamauchi-t@plus-agc.com>
2020-09-09 12:29:40 +0900

    Update mail.php

--- recruit/entry/mail.php
+++ recruit/entry/mail.php
@@ -30,14 +30,14 @@ if ( version_compare( PHP_VERSION, '5.1.0', '>=' ) ) {//PHP5.1.0以上の場合�
 //---------------------------　必須設定　必ず設定してください　-----------------------
 
 //サイトのトップページのURL　※デフォルトでは送信完了後に「トップページへ戻る」ボタンが表示されますので
-$site_top = "https://maruyoshi-ironworks.com/";
+$site_top = "https://maruyoshi-ironworks.com/recruit/";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
-$to = "infomation@maruyoshi-ironworks.com,yamauchi-t@plus-agc.com";
+$to = "yamauchi-t@plus-agc.com";
 
 //自動返信メールの送信元メールアドレス
 //必ず実在するメールアドレスでかつ出来る限り設置先サイトのドメインと同じドメインのメールアドレスとすることを強く推奨します
-$from = "infomation@maruyoshi-ironworks.com,yamauchi-t@plus-agc.com";
+$from = "yamauchi-t@plus-agc.com";
 
 //フォームのメールアドレス入力箇所のname属性の値（name="○○"　の○○部分）
 $Email = "Eメール";
@@ -52,7 +52,7 @@ $Referer_check = 0;
 
 //リファラチェックを「する」場合のドメイン ※設置するサイトのドメインを指定して下さい。
 //もしこの設定が間違っている場合は送信テストですぐに気付けます。
-$Referer_check_domain = "https://maruyoshi-ironworks.com/";
+$Referer_check_domain = "https://maruyoshi-ironworks.com/recruit/";
 
 /*セッションによるワンタイムトークン（CSRF対策、及びスパム防止）(する=1, しない=0)
 ※ただし、この機能を使う場合は↓の送信確認画面の表示が必須です。（デフォルトではON（1）になっています）
@@ -84,7 +84,7 @@ $confirmDsp = 1;
 $jumpPage = 1;
 
 // 送信完了後に表示するページURL（上記で1を設定した場合のみ）※httpから始まるURLで指定ください。（相対パスでも基本的には問題ないです）
-$thanksPage = "https://maruyoshi-ironworks.com/recruit/";
+$thanksPage = "https://maruyoshi-ironworks.com/recruit/thanks.html";
 
 // 必須入力項目を設定する(する=1, しない=0)
 $requireCheck = 1;
